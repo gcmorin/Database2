3b142980f02a72b19475fd4433414fda7d685bda
Gage <79539411+gcmorin@users.noreply.github.com>
2022-02-17 12:08:09 -0500

    Update DB2.sql

--- DB2.sql
+++ DB2.sql
@@ -20,6 +20,27 @@ create table user (
 	primary key (id)
 )AUTO_INCREMENT=1;
 
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+insert into user values ();
+
 create table publisher (
 	login char(20),
 	password char(20),
@@ -28,6 +49,12 @@ create table publisher (
 	constraint foreign key (publisher_id) references user(id),
 	primary key (login)
 );
+
+insert into publisher values ("pub1", "Test123", 1, "Great Books");
+insert into publisher values ("pub2", "Test123", 2, "Cool Writing");
+insert into publisher values ("pub3", "Test123", 3, "Poem Boys");
+insert into publisher values ("pub4", "Test123", 4, "Car Magazine");
+insert into publisher values ("pub5", "Test123", 5, "Bible Studies");
 
 create table admin (
 	login char(20),
@@ -38,10 +65,23 @@ create table admin (
 	primary key (login)
 );
 
+insert into admin values("admin1", "AdLogin123", 6, "Shane");
+insert into admin values("admin2", "AdLogin123", 7, "Gage");
+insert into admin values("admin3", "AdLogin123", 8, "Carl");
+insert into admin values("admin4", "AdLogin123", 9, "Dan");
+insert into admin values("admin5", "AdLogin123", 10, "Tom");
+
+
 create table customer (
 	customer_id int(20),
 	constraint foreign key (customer_id) references user(id)
 );
+
+insert into customer values (11);
+insert into customer values (12);
+insert into customer values (13);
+insert into customer values (14);
+insert into customer values (15);
 
 create table account (
 	login char(20),
@@ -50,10 +90,12 @@ create table account (
 	name char(255),
 	address char(255),
 	email char(255),
-	memberstatus bool,
+	memberstatus bool DEFAULT 0,
 	constraint foreign key (customer_id) references customer(customer_id),
 	primary key (login)
 );
+
+insert into account values ("cust1", "CLogin123", 11, "Sam", "121 Earth St.", "sam@gmail.com", '');
 
 create table author (
 	author_id int,
@@ -117,7 +159,6 @@ create table cart (
 	constraint foreign key (order_id) references orders(order_id),
 	primary key (cart_id)	
 )AUTO_INCREMENT=1;
-
 
 
 
